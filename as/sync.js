decaf635b24be9bba2513da36766f22109d450ba
Lucas <lucas.silva@agenciamilagro.com.br>
2014-01-24 10:49:10 -0200

    Update sync

--- as/sync.js
+++ as/sync.js
@@ -2,6 +2,6 @@ var fs = require('fs');
 
 for (var i = 1; i <= 5; i++) {
   var file = "sync" + i + ".markdown";
-  var out = file.writeFileSync(file, "###Sync");
+  var out = fs.writeFileSync(file, "###Sync");
   console.log(out);
 }
